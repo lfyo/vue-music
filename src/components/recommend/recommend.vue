fe283eb03de8798a74242a9f865c270883cb9be1
lufangyong <lu1329@qq.com>
2018-02-16 12:38:36 +0800

    add slider

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,13 +1,109 @@
 <template>
-  <div>
-    推荐页面
+  <div class="recommend">
+    <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+      <slider>
+        <div v-for="(item, index) in recommends" :key="index">
+          <a :href="item.linkUrl">
+            <img class="needsclick" @load="loadImage" :src="item.picUrl">
+          </a>
+        </div>
+      </slider>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from '../../base/slider/slider'
+  import {ERR_OK} from '../../api/config'
+  import {getRecommend} from '../../api/recommend'
 
+  export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
+    components: {
+      Slider
+    },
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+
+        this.$refs.recommend.style.bottom = bottom
+      },
+      loadImage() {
+        if (!this.checkloaded) {
+          this.checkloaded = true
+        }
+      },
+      selectItem(item) {
+        this.$router.push({
+          path: `/recommend/${item.dissid}`
+        })
+        this.setDisc(item)
+      },
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
 
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
